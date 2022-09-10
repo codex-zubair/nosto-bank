9920a2e7d7070f6c64fe8bd0e1cb75cdcf2225a3
Rocko <codingwithrock@gmail.com>
2022-09-10 15:30:53 +0600

    fixed all negative value

--- js/deposit.js
+++ js/deposit.js
@@ -8,44 +8,51 @@ document.getElementById("btn-deposit").addEventListener("click", function () {
     3. convert string value to a number
     */
   const newDepositAmount = document.getElementById("deposit-field").value;
-  if (isNaN(newDepositAmount)) {
-    alert("Only Number!");
-    document.getElementById("deposit-field").value = ``;
+  if(parseFloat(newDepositAmount) < 0)
+  {
+    alert("Only valid Number!");
   }
-
-  else {
-
-    /* 
-        1. get previous deposit total by id
-    
-        */
-
-
-    const previousDepositTotal = document.getElementById("balance-total").innerText;
-
-
-
-
-
-
-    // calculate new deposit total
-    const newDepositTotal = parseFloat(previousDepositTotal) + parseFloat(newDepositAmount);
-
-
-
-
-
-    // set deposit total value
-    document.getElementById("deposit-total").innerText = newDepositAmount;
-
-
-    // get previous balance by using the function
-
-
-    document.getElementById("balance-total").innerText = newDepositTotal;
-    document.getElementById("deposit-field").value = ``;
-
-
+  else
+  {
+    if (isNaN(newDepositAmount)) {
+      alert("Only Valid Number!");
+      document.getElementById("deposit-field").value = ``;
+    }
+  
+    else {
+  
+      /* 
+          1. get previous deposit total by id
+      
+          */
+  
+  
+      const previousDepositTotal = document.getElementById("balance-total").innerText;
+  
+  
+  
+  
+  
+  
+      // calculate new deposit total
+      const newDepositTotal = parseFloat(previousDepositTotal) + parseFloat(newDepositAmount);
+  
+  
+  
+  
+  
+      // set deposit total value
+      document.getElementById("deposit-total").innerText = newDepositAmount;
+  
+  
+      // get previous balance by using the function
+  
+  
+      document.getElementById("balance-total").innerText = newDepositTotal;
+      document.getElementById("deposit-field").value = ``;
+  
+  
+    }
   }
 
 });
